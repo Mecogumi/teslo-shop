8938f34a1511479f4f9b96601c43015edde1f802
Jose Damian Gomez Hernandez <jose.gomez@correo.uady.mx>
2025-07-03 10:39:16 -0600

    Added a simple cache for pagination

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product, ProductsResponse } from '../interfaces/product-response.interface';
-import { Observable } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 const apiUrl = environment.baseUrl
@@ -14,11 +14,20 @@ interface Options {
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
   private http = inject(HttpClient)
+
+  private productCache = new Map<string, ProductsResponse>()
+
   getProducts(options: Options): Observable<ProductsResponse> {
     const { limit = 10, offset = 0, gender = '' } = options;
+    const key = `${limit}-${offset}-${gender}`
+    if (this.productCache.has(key)) {
+      return of(this.productCache.get(key)!);
+    }
     return this.http.get<ProductsResponse>(`${apiUrl}/products`, {
       params: { limit, offset, gender }
-    })
+    }).pipe(
+      tap(resp => this.productCache.set(key, resp))
+    )
   }
 
   getProduct(query: string) {
